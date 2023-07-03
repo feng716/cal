cc32d4273e84418d2a2e0615e5d312e4c0d786f8
feng716 <qq3161712318_FENG@outlook.com>
2023-07-03 23:40:28 +0800

    fix interval

--- cal.py
+++ cal.py
@@ -8,7 +8,7 @@ class ring:
         self.sa=lambda x,*args:S*(x[p]+x[1+p])/x[p+1]+2*3.14*(x[1+p]*x[1+p]-x[p]*x[p])
         
         # limit
-        self.lim1=lambda x:-sqrt(x[2+p]/x[2+p])+0.03
+        self.lim1=lambda x:-sqrt(x[2+p]/x[1+p])+0.03
         self.con1={"type":"ineq","fun":self.lim1}
         self.lim4=lambda x:x[1+p]-1000*x[2+p]/9
         self.con4={"type":"ineq","fun":self.lim4}
@@ -16,13 +16,16 @@ class ring:
         self.con5={"type":"ineq","fun":self.lim5}
         self.lim6=lambda x:x[1+p]-x[p]-70
         self.con6={"type":"ineq","fun":self.lim6}
-        self.cons=(self.con1,self.con4,self.con5,self.con6)
-        self.bounds=((0,None),(0,None),(4.9,7.84),(None,None))
+        self.lim7=lambda x:x[p+3]-sqrt(x[p+2]/x[p+1])
+        self.con7={"type":"eq","fun":self.lim7}
+        self.cons=(self.con1,self.con4,self.con5,self.con6,self.con7)
+        self.bounds=((0,None),(0,None),(4.9,7.84),(-0.03,0.03))
         self.args=(500,600,6,0.02)
         self.m=m
         self.p=p
         self.S=S
         self.rList.append(self)
+        self.id=len(self.rList)
 def s(rings):
     # return lambda x:rings[0].sa(x)+rings[1].sa(x)+rings[2].sa(x)+rings[3].sa(x)
     def func(x):
@@ -59,7 +62,32 @@ def gBound(rs):
     for i in range(0,4):
         a+=rs[i].bounds
     return a
-
+def gInterval(rs):
+    def interval(x):
+        a={}
+        for i in rs:
+            a[x[i.p]]=i.id# R1
+            a[x[i.p+1]]=i.id# R2
+        lst=sorted(a)
+        activeID=[]
+        length=0
+        for element in lst:
+            if(a[element] in activeID):
+                del activeID[activeID.index(a[element])]
+            else:
+                activeID.append(a[element])
+            current_r1=x[rs[a[element]-1].p]
+            current_r2=x[rs[a[element]-1].p+1]
+            for i in activeID:
+                if(i!=a[element]):
+                    r1=x[rs[i-1].p]
+                    r2=x[rs[i-1].p+1]
+                    r1=current_r1 if(r1<current_r1) else r1
+                    r2=current_r2 if(r2>current_r2) else r2
+                    length+=r2-r1
+        return length/2
+    return interval
+con_eq+=({"type":"eq","fun":gInterval(ring.rList)},)
 residential = ring(1369240,10000,0)
 tra_in=ring(1620240,90000,4)
 arg=ring(235206,10000,8)
